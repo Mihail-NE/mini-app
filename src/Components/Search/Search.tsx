11de813f5d7b2dbf30a6a692906aadcda9d74203
Mihail <harkunovmihail953@gmail.com>
2024-08-13 11:51:34 +0300

    fix serch component

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,6 +1,6 @@
 import s from "./Search.module.scss";
 import SearchIcon from "../../assets/icon-search";
-import { useRef } from "react";
+
 import Button from "../Button/Button";
 
 interface SearchProps {
@@ -8,18 +8,18 @@ interface SearchProps {
     onSubmit: (tetx: string) => void;
 }
 
+
 const Search = ({ hasError, onSubmit }: SearchProps) => {
-    const searchRef = useRef<HTMLInputElement | null>(null);
 
-    const handleubmit = (e: React.FormEvent) => {
+    const handleubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const text = searchRef.current ? searchRef.current.value : "";
-
+        const text = (e.target as HTMLFormElement).search.value;
+        console.log(text);
         if (text) {
-            onSubmit(text);
-            if (searchRef.current) searchRef.current.value = "";
+           onSubmit(text);
+           (e.target as HTMLFormElement).search.value = "";
         }
-    };
+    }
 
     return (
         <form onSubmit={handleubmit} autoComplete="off">
@@ -28,7 +28,6 @@ const Search = ({ hasError, onSubmit }: SearchProps) => {
                     <SearchIcon className={s.icon} />
                 </label>
                 <input
-                    ref={searchRef}
                     type="text"
                     className={s.textField}
                     id="search"
