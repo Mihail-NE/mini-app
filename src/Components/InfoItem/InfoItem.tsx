daeb76a4ef0e71582cb581be042de14967bfb1d3
Mihail <harkunovmihail953@gmail.com>
2024-08-15 15:11:43 +0300

    add infoItem component

---
+++ src/Components/InfoItem/InfoItem.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import s from "./InfoItem.module.scss";
+
+interface InfoItemProps {
+    icon: React.ReactNode;
+    text?: string | null;
+    isLink?: boolean;
+}
+
+const InfoItem = ({ icon, text, isLink }: InfoItemProps) => {
+    const currentText = text || "Not Available";
+    let currentHref = '';
+
+    if (isLink) {
+        currentHref = text && text.startsWith("http") ? text : `https${text}`;
+    }
+
+    return (
+        <div className={`${s.infoItem}${text ? "" : `${s.empty}`}`}>
+            {icon}
+            <div>
+                {isLink && text ? (
+                    <a
+                    href="currentHref"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={s.link}
+                    >
+                        currentText
+                    </a>
+                ) : currentText}
+            </div>
+        </div>
+    );
+};
+
+export default InfoItem;
